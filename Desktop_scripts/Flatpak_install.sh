554455998fd918e4263c283b515db711d61a0135
Atemo Cajaku <160250128+Atemo-C@users.noreply.github.com>
2024-05-11 13:24:37 +0000

    Create `Flatpak_install.sh`

---
+++ Desktop_scripts/Flatpak_install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+flatpak install flathub --user \
+	com.ultimaker.cura \
+	com.userbottles.bottles \
+	com.vysp3r.RetroPlus \
+	dev.vencord.Vesktop \
+	net.pcsx2.PCSX2 \
+	net.rpcs3.RPCS3 \
+	org.vinegarhq.Vinegar \
+	page.codeberg.JakobDev.jdNBTExplorer \
+	re.sonny.Workbench \
+	xyz.ketok.Speedtest -y
