ab4a4ab84554afdadd844fccc430208c10ad5308
Atemo Cajaku <160250128+Atemo-C@users.noreply.github.com>
2024-05-14 23:05:00 +0000

    Fixed two bugs.
    ## Bug 1:
    Hyprpaper sometimes did not have enough time to get killed before trying to get started again.
    
    ## Bug 2:
    When Hypraper was not already running, the script would not work.

--- Desktop_scripts/Hyprpaper.sh
+++ Desktop_scripts/Hyprpaper.sh
@@ -13,8 +13,9 @@ Wallpaper=$(zenity \
 case $? in
 	0) echo -e \
 		"preload = $Wallpaper\nwallpaper = ,$Wallpaper\nsplash = false\nipc = off" \
-		> "$HOME/.config/hypr/hyprpaper.conf" && \
-		killall hyprpaper ;
+		> "$HOME/.config/hypr/hyprpaper.conf" ;
+		killall hyprpaper || true ;
+		sleep 1 &&
 		hyprpaper & disown;;
 	1) notify-send "No wallpaper has been selected.";;
 	-1) notify-send "An unexpected error has occured - Run this program from the terminal.";;
