4b134fd400f38e4549706340a6547132cead371c
Atemo-C <c.roux1993@gmail.com>
2024-06-29 11:49:24 +0200

    Audacity now uses X11, because it is weird on Wayland.

--- Desktop scripts/Program launcher.sh
+++ Desktop scripts/Program launcher.sh
@@ -131,8 +131,8 @@ elif [ "$Program" = "  Audacious            Music player" ]; then
 	audacious & disown
 
 elif [ "$Program" = "  Audacity             Audio editor" ]; then
-#	GDK_BACKEND=x11 audacity & disown
-	audacity & disown
+	GDK_BACKEND=x11 audacity & disown
+#	audacity & disown
 
 elif [ "$Program" = "  EasyEffects          Effects to inputs or outputs" ]; then
 	easyeffects & disown
